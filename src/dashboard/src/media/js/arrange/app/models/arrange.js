945963b662d5729e2776c335981ff8f53b8be44c
Holly Becker <hbecker@artefactual.com>
2015-04-26 13:58:17 -0700

    Ember: attributes from model

--- src/dashboard/src/media/js/arrange/app/models/arrange.js
+++ src/dashboard/src/media/js/arrange/app/models/arrange.js
@@ -1,23 +1,33 @@
 
 Arrange.Directory = DS.Model.extend({
    name: DS.attr('string'),
-   open: DS.attr('boolean')
+   open: DS.attr('boolean'),
+   type: DS.attr('string'),
 });
 
 Arrange.Directory.FIXTURES = [
     {
         id: 1,
         name: 'sip-uuid',
-        open: false
+        open: false,
+        type: 'directory',
     },
     {
         id: 2,
         name: 'open-sip-uuid',
-        open: true
+        open: true,
+        type: 'directory',
     },
     {
         id: 3,
         name: 'another sip',
-        open: true
+        open: true,
+        type: 'directory',
+    },
+    {
+        id: 4,
+        name: 'look a file',
+        open: null,
+        type: 'file',
     }
 ]
